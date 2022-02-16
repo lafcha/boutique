779a7126a2937265d9d7400f83cf538ea4b17445
lafcha <charlotte.bronsard@gmail.com>
2022-02-16 10:27:43 +0100

    fusion of css files

--- resources/views/layouts/footer.tpl.php
+++ resources/views/layouts/footer.tpl.php
@@ -1,9 +1,3 @@
-<head>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
-          integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <link rel="stylesheet" href="/resources/css/footer.css">
-</head>
-
 <footer class="bg-primary py-2 container-fluid">
     <div class=" d-flex flex-md-row justify-content-md-between flex-column align-items-center">
         <div class="d-flex flex-column align-items-center text-center py-3 text-success w-50">
