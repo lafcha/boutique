4382732ec6eeb5f0cc8299771207e4916421e3b9
vbruchon <vivian.brcuhon@le-campus-numerique.fr>
2022-02-16 09:28:03 +0100

    Dynamique link & create toogle cart

--- resources/views/layouts/footer.tpl.php
+++ resources/views/layouts/footer.tpl.php
@@ -1,36 +1,41 @@
-<head><link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-<link rel="stylesheet" href="/resources/css/footer.css">
+<head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="/resources/css/footer.css">
 </head>
 
 <footer class="bg-primary py-2 container-fluid">
     <div class=" d-flex flex-md-row justify-content-md-between flex-column align-items-center">
-      <div class="d-flex flex-column align-items-center text-center py-3 text-success w-50">
-        <a class="text-success text-decoration-none" href="">Nos pains</a>
-        <a class="text-success text-decoration-none" href="">Nos Viennoiseries</a>
-        <a class="text-success text-decoration-none" href="">Nos Pâtisseries</a>
-        <a class="text-success text-decoration-none" href="">Nos Snacks</a>
-        <a class="text-success text-decoration-none" href="">Nos Boissons</a>
-      </div>
-      <div class="bg-success d-flex flex-column align-items-center text-center p-3 m-3 w-50">
-        <p class="text-secondary text-uppercase">Nous Contacter</p>
-        <p class="text-primary">06 10 20 30 40</p>
-        <a class="text-primary text-decoration-none" href="">email@email.fr</a>
-      </div>
-      <div class="row justify-content-center py-3 w-50">
-        <a class="col-6 col-lg-4 d-flex justify-content-center" href="https://fr.linkedin.com/"><img
-            src="/resources/img/linkedin.png" alt="Lien Linkedin" class="w-50"></a>
-        <a class="col-6 col-lg-4 d-flex justify-content-center" href="https://www.instagram.com/?hl=fr"><img
-            src="/resources/img/instagram.png" alt="Lien Instagram" class="w-50"></a>
-        <a class="col-6 col-lg-4 d-flex justify-content-center" href="https://twitter.com/?lang=fr"><img
-            src="/resources/img/twitter.png" alt="Lien Twitter" class="w-50"></a>
-      </div>
+        <div class="d-flex flex-column align-items-center text-center py-3 text-success w-50">
+            <a class="text-success text-decoration-none" href="">Nos pains</a>
+            <a class="text-success text-decoration-none" href="">Nos Viennoiseries</a>
+            <a class="text-success text-decoration-none" href="">Nos Pâtisseries</a>
+            <a class="text-success text-decoration-none" href="">Nos Snacks</a>
+            <a class="text-success text-decoration-none" href="">Nos Boissons</a>
+            <a href="http://boutique.local/?action=cart"><div class="cart-pill2"></div></a>
+        </div>
+
+        <div class="bg-success d-flex flex-column align-items-center text-center p-3 m-3 w-50">
+            <p class="text-secondary text-uppercase">Nous Contacter</p>
+            <p class="text-primary">06 10 20 30 40</p>
+            <a class="text-primary text-decoration-none" href="">email@email.fr</a>
+        </div>
+
+        <div class="row justify-content-center py-3 w-50">
+            <a class="col-6 col-lg-4 d-flex justify-content-center" href="https://fr.linkedin.com/"><img
+                        src="/resources/img/linkedin.png" alt="Lien Linkedin" class="w-50"></a>
+            <a class="col-6 col-lg-4 d-flex justify-content-center" href="https://www.instagram.com/?hl=fr"><img
+                        src="/resources/img/instagram.png" alt="Lien Instagram" class="w-50"></a>
+            <a class="col-6 col-lg-4 d-flex justify-content-center" href="https://twitter.com/?lang=fr"><img
+                        src="/resources/img/twitter.png" alt="Lien Twitter" class="w-50"></a>
+        </div>
     </div>
     <div>
-      <a class="px-3 text-success text-decoration-none" href="Mentions_legales.html">@2021 - Mentions Légales</a>
+        <a class="px-3 text-success text-decoration-none" href="Mentions_legales.html">@2021 - Mentions Légales</a>
     </div>
-  </footer>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
-  integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
-  crossorigin="anonymous"></script>
+</footer>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
+        crossorigin="anonymous"></script>
 </body>
 </html>
--- resources/views/layouts/header.tpl.php
+++ resources/views/layouts/header.tpl.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
           integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-
+    <script src="https://code.iconify.design/2/2.1.2/iconify.min.js"></script>
     <link rel="stylesheet" href="/resources/css/header.css">
 
     <title>Document</title>
@@ -17,7 +17,7 @@
     <div id="logo"><img class="w-75" src="/resources/img/logoboutique.png" alt="Logo"></div>
     <div id="navbar">
 
-        <nav class="navbar navbar-expand-lg navbar-light d-flex flex-row   translate-middle-y mt-5">
+        <nav class="navbar navbar-expand-lg navbar-light d-flex flex-row flex translate-middle-y mt-5">
             <ul class="navbar-nav">
                 <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle text-success" href="#" id="navbarDropdownMenuLink" role="button"
@@ -25,8 +25,10 @@
                         Nos pains
                     </a>
                     <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                        <?php foreach ($breads as $rows => $bread){?>
-                        <li><a class="dropdown-item" href="/app/controllers/productPageController.php"><?= $bread['name']?></a></li>
+                        <?php foreach ($breads as $rows => $bread) { ?>
+                            <li><a class="dropdown-item"
+                                   href="http://boutique.local/?action=productPage&id=<?= $filterId ?>"><?= $bread['name'] ?></a>
+                            </li>
                         <?php } ?>
                     </ul>
                 </li>
@@ -36,8 +38,10 @@
                         Nos Viennoiseries
                     </a>
                     <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                        <?php foreach ($viennoiseries as $rows => $viennoiserie){?>
-                            <li><a class="dropdown-item" href="#"><?= $viennoiserie['name']?></a></li>
+                        <?php foreach ($viennoiseries as $rows => $viennoiserie) { ?>
+                            <li><a class="dropdown-item"
+                                   href="http://boutique.local/?action=productPage&id=<?= $filterId ?>"><?= $viennoiserie['name'] ?></a>
+                            </li>
                         <?php } ?>
                     </ul>
                 </li>
@@ -47,8 +51,10 @@
                         Nos Pâtisseries
                     </a>
                     <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                        <?php foreach ($pastries as $rows => $pastrie){?>
-                        <li><a class="dropdown-item" href="#"><?= $pastrie['name']?></a></li>
+                        <?php foreach ($pastries as $rows => $pastrie) { ?>
+                            <li><a class="dropdown-item"
+                                   href="http://boutique.local/?action=productPage&id=<?= $filterId ?>"><?= $pastrie['name'] ?></a>
+                            </li>
                         <?php } ?>
                     </ul>
                 </li>
@@ -58,8 +64,10 @@
                         Nos Snacks
                     </a>
                     <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                        <?php foreach ($snacks as $rows => $snack){?>
-                        <li><a class="dropdown-item" href="#"><?= $snack['name']?></a></li>
+                        <?php foreach ($snacks as $rows => $snack) { ?>
+                            <li><a class="dropdown-item"
+                                   href="http://boutique.local/?action=productPage&id=<?= $filterId ?>"><?= $snack['name'] ?></a>
+                            </li>
                         <?php } ?>
                     </ul>
                 </li>
@@ -69,11 +77,16 @@
                         Nos Boissons
                     </a>
                     <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                        <?php foreach ($drinks as $rows => $drink){?>
-                        <li><a class="dropdown-item" href="#"><?= $drink['name']?></a></li>
+                        <?php foreach ($drinks as $rows => $drink) { ?>
+                            <li><a class="dropdown-item"
+                                   href="http://boutique.local/?action=productPage&id=<?= $filterId ?>"><?= $drink['name'] ?></a>
+                            </li>
                         <?php } ?>
                     </ul>
                 </li>
+
+                <li><a href="http://boutique.local/?action=cart"><div class="cart-pill2"></div></a></li>
+
             </ul>
         </nav>
     </div>
@@ -84,4 +97,4 @@
 <li><a class="dropdown-item" href="#">Flûte </a></li>
 <li><a class="dropdown-item" href="#">Pain de Mie</a></li>
 <li><a class="dropdown-item" href="#">Pain aux Céréales</a></li>
-<li><a class="dropdown-item" href="#">Pain Complet</a></li>*/?>
+<li><a class="dropdown-item" href="#">Pain Complet</a></li>*/ ?>
